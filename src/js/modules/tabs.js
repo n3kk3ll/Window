c0592700f0d3b8c71af1e54d8cee880c5d913412
Zhemchuzhnyi Andrii <n3kk3ll@gmail.com>
2022-05-29 14:20:56 +0300

    added tabs functionality

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,36 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+  const header = document.querySelector(headerSelector),
+    tabs = document.querySelectorAll(tabSelector),
+    content = document.querySelectorAll(contentSelector);
+
+  function hideTabContent() {
+    content.forEach(content => {
+      content.style.display = `none`;
+    });
+
+    tabs.forEach(tab => {
+      tab.classList.remove(activeClass);
+    });
+  }
+
+  function showTabContent(index = 0) {
+    content[index].style.display = `block`;
+    tabs[index].classList.add(activeClass);
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  header.addEventListener(`click`, e => {
+    if(e.target && (e.target.classList.contains(tabSelector.replace(/\./, ``)) || e.target.parentNode.classList.contains(tabSelector.replace(/\./, ``)))) {
+      tabs.forEach((tab, index) => {
+        if(e.target === tab || e.target.parentNode === tab) {
+          hideTabContent();
+          showTabContent(index);
+        }
+      });
+    }
+  });
+};
+
+export default tabs;
